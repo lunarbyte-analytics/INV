efbce597e0fd11251a36c8e10f81eef536b5195d
Pawel <pawel.sawukajtys@gmail.com>
2026-03-23 22:45:39 +0100

    Initialize application settings loading in main function
    - Integrated load_settings() from app_env to initialize application environment settings at the start of the main application.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+from .app_env import load_settings
 from .db import init_db
 from .ui.main_app import MainApp
 
+
 def main():
+    load_settings()
     init_db()
     try:
         from ctypes import windll
